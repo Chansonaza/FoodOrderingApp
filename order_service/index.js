3380aac84ebd0478d3ada54f2ea1ab46d683e42d
Chansonaza <156300087+Chansonaza@users.noreply.github.com>
2026-09-01 16:47:03 +0700

    Update index.js

--- order_service/index.js
+++ order_service/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 const PORT = 3002;
 const PAYMENT_SERVICE_URL = 'http://3.88.201.241:3003/paymentprocess';
-const NOTIFICATION_SERVICE_URL = 'http://3.94.127.200:3004/sendnotification';
+const NOTIFICATION_SERVICE_URL = 'http://localhost:3004/sendnotification';
 
 // In-memory order store
 let orders = [];
